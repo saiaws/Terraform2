74e32db092e719f647a00fd682842bbc7b65869c
SaiKrishna <30415103+saiaws@users.noreply.github.com>
2019-04-22 14:50:43 +0530

    Update base.tf

--- base.tf
+++ base.tf
@@ -4,9 +4,12 @@
 
 #################################################################
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
+variable "aws_access_key" {
+	}
+variable "aws_secret_key" {
+	}
+variable "aws_region" {
+	}
 
 variable "key_name" {
 	default = "project"
